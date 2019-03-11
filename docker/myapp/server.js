a752afcafc916041a514a991509aeb79b96d029c
RatanBekal <ratankb@outlook.com>
2019-03-11 22:43:35 +0530

    Update server.js

--- docker/myapp/server.js
+++ docker/myapp/server.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
-
 app.get('/', function (req, res) {
-  res.send('Hello Fargate!');
+  res.send('Hello World! from NodeJS App - Build 1.0.0.30');
 });
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
